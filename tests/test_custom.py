84944fe86c196952e81a2e7fa5d9371a105653fc
Tabish Shaikh <tabish@archive.org>
2021-09-21 22:12:17 +0530

    feat: adding custom tests and StrategyResolvers

--- tests/test_custom.py
+++ tests/test_custom.py
@@ -11,5 +11,46 @@ from helpers.time import days
 """
 
 
-def test_my_custom_test(deployed):
-    assert True
+def test_custom_harvest(deployer, vault, strategy, want):
+    # Setup
+    startingBalance = want.balanceOf(deployer)
+    reward = interface.IERC20(strategy.reward())
+
+    depositAmount = startingBalance // 2
+    assert startingBalance >= depositAmount
+    assert startingBalance >= 0
+    # End Setup
+    print("Setup Complete")
+
+    # Deposit
+    # Before deposit we want balance of vault to be 0
+    assert want.balanceOf(vault) == 0
+
+    want.approve(vault, MaxUint256, {"from": deployer})
+    vault.deposit(depositAmount, {"from": deployer})
+
+    available = vault.available()
+    # After deposit check if available balance > 0
+    assert available > 0
+    print("Avaiable amount in vault: ", vault.available())
+
+    # deposit into strat
+    vault.earn({"from": deployer})
+
+    chef = Contract.from_explorer(strategy.CHEF())
+    poolId = strategy.pid()
+    (amount, _) = chef.userInfo(poolId, strategy.address)
+    # check if deposited amount is > 0
+    assert amount > 0
+
+    chain.sleep(days(1))  # sleep for a day
+    chain.mine(500)  # Mine so we get some interest
+
+    # Harvest rewards
+    strategy.harvest({"from": deployer})
+
+    # Strategy should have some want after swapping rewards of harvest
+    assert strategy.balanceOfWant() > 0
+    assert strategy.isTendable()
+    # Strategy shouldn't have any rewards left
+    assert reward.balanceOf(strategy) == 0
